dcdbb5c126a7f20516a877001bd2a83552f45d28
barlowa <35688619+barlowa@users.noreply.github.com>
2022-05-09 22:42:56 +0100

    basic data fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,25 @@
+import { useQuery, gql } from '@apollo/client';
+
 export default function BlogListingPage() {
-  return <div>...</div>;
+  const q = useQuery(gql`
+    query GetPosts {
+      blogPostCollection {
+        # skip
+        # limit
+        # total
+        items {
+          title
+          # preface
+          # body
+        }
+      }
+    }
+  `);
+
+  console.log(q);
+  if (q.loading) return <div>Loading...</div>;
+
+  if (q.error) return <div>{q.error?.message}</div>;
+
+  return <div>{JSON.stringify(q.data)}</div>;
 }
