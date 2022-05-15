a4f053ae3c3b5e94fbeafc642315e0c968bda684
barlowa <35688619+barlowa@users.noreply.github.com>
2022-05-16 00:31:02 +0100

    add tsconfig mappings to jest config

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,10 @@ module.exports = createJestConfig({
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['./jest.setup.ts'],
+  moduleNameMapper: {
+    '^components/(.*)$': ['<rootDir>/src/components/$1'],
+    '^layouts/(.*)$': ['<rootDir>/src/layouts/$1'],
+    '^styles/(.*)$': ['<rootDir>/src/styles/$1'],
+    '^types/(.*)$': ['<rootDir>/src/types/$1'],
+  },
 });
