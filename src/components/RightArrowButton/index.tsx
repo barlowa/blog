568f98887f3831212855e0d58e73e74d1a6f4bc4
barlowa <35688619+barlowa@users.noreply.github.com>
2022-05-15 18:49:39 +0100

    create a right arrow button component

---
+++ src/components/RightArrowButton/index.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export interface IRightArrowButtonProps {
+  onClick?: () => void;
+  width?: string;
+  height?: string;
+}
+
+export default function RightArrowButton(props: IRightArrowButtonProps) {
+  return (
+    <StyledButton>
+      <RightArrowStyle
+        xmlns="http://www.w3.org/2000/svg"
+        id="right-arrow"
+        viewBox="0 0 150 50"
+        version="1.1"
+        y="0px"
+        x="0px"
+        height={props?.height}
+        width={props?.width}
+      >
+        <g>
+          <line y2="24.704" x1="50" x2="125.3" y1="24.704" strokeWidth="6" />
+          <polygon points="124.4 6.284 124.4 44.606 148.35 23.69" />
+        </g>
+      </RightArrowStyle>
+    </StyledButton>
+  );
+}
+
+const RightArrowStyle = styled.svg`
+  fill: ${({ theme: { colours } }) => colours.sky};
+  stroke: ${({ theme: { colours } }) => colours.sky};
+  height: ${({ height }) => height};
+  width: ${({ width }) => width};
+`;
+
+const StyledButton = styled.button`
+  display: flex;
+  background-color: transparent;
+  margin: 0;
+  padding: 10px;
+  border: none;
+  cursor: pointer;
+`;
