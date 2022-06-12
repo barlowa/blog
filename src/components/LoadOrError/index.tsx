e0f2c3f3fc33836cd67e35d1d6353f181ce6e955
barlowa <35688619+barlowa@users.noreply.github.com>
2022-06-12 19:34:37 +0100

    added styling to error and loading states

--- src/components/LoadOrError/index.tsx
+++ src/components/LoadOrError/index.tsx
@@ -1,18 +1,74 @@
+import styled, { css } from 'styled-components';
 interface ILoadOrErrorProps {
   loading: boolean;
   error?: { message: string };
   children: React.ReactNode;
 }
 
-const LoadOrError = ({ loading, error, children }: ILoadOrErrorProps) => {
+const LoadOrError = ({
+  loading = true,
+  error,
+  children,
+}: ILoadOrErrorProps) => {
   if (loading) {
-    return <p>Loading...</p>;
+    return (
+      <Wrapper>
+        <Spinner />
+      </Wrapper>
+    );
   }
   if (error) {
-    return <p>{error.message}</p>;
+    return (
+      <Wrapper>
+        <ErrorText bold addPadding>
+          Error:
+        </ErrorText>
+        <ErrorText>{error.message}</ErrorText>
+      </Wrapper>
+    );
   }
 
   return <>{children}</>;
 };
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Spinner = styled.div`
+  border: 0.5em solid ${({ theme: { colours } }) => colours.white};
+  border-top: 0.5em solid ${({ theme: { colours } }) => colours.blue};
+  border-radius: 50%;
+  width: 2em;
+  height: 2em;
+  animation: spin 0.75s linear infinite;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const ErrorText = styled.p`
+  ${({ bold }) =>
+    bold &&
+    css`
+      font-weight: bold;
+    `}
+  ${({ addPadding }) =>
+    addPadding &&
+    css`
+      padding: 0.25em;
+    `}
+  margin: 0;
+  color: red;
+`;
 
 export default LoadOrError;
